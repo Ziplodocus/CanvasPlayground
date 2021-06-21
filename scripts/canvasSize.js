0bd6b24d09c79e82a75d42c2ccbd1eaa9a7c810e
Harry-Purple <haroldholehouse@gmail.com>
2021-06-21 14:12:42 +0100

    add canvas resizing and placeholder collisions

--- scripts/canvasSize.js
+++ scripts/canvasSize.js
@@ -1,12 +1,43 @@
+let resizeId;
+let canvasSize = {
+    width: getComputedStyle(canvas).width.replace('px',''),
+    height: getComputedStyle(canvas).height.replace('px',''),
+    area() {
+        return this.width * this.height
+    }
+}
 
 function resizeCanvas() {
-    canvas
-    let canvasStyle = getComputedStyle(canvas);
-    let canvasHeight = canvasStyle.height;
-    let canvasWidth = canvasStyle.width;
-    canvas.height = canvasHeight;
-    canvas.width = canvasWidth;
+
+    let newCanvasSize = {
+        width: getComputedStyle(canvas).width.replace('px',''),
+        height: getComputedStyle(canvas).height.replace('px',''),
+        area() {
+            return this.width * this.height
+        }
+    }
+
+
+    particles.forEach(particle => {
+        particle._x = particle.x * (newCanvasSize.width / canvasSize.width);
+        particle._y = particle.y * (newCanvasSize.height / canvasSize.height);
+        let sizeRatio = newCanvasSize.area() / canvasSize.area();
+        particle._vicinity * sizeRatio;
+        particle._radius * sizeRatio;
+    })
+ 
+
+    canvas.height = Math.floor(resolutionModifier * newCanvasSize.height);
+    canvas.width = Math.floor(resolutionModifier * newCanvasSize.width);
+
+    canvasSize = newCanvasSize;
+    
+}
+
+function resize() {
+    clearTimeout(resizeId);
+    resizeId = setTimeout(resizeCanvas, 500);
 }
 
 resizeCanvas();
-window.addEventListener('resize', resizeCanvas);
+window.addEventListener('resize', resize);
--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,19 @@
 
 const canvas = document.getElementById('playground');
 const ctx = canvas.getContext('2d');
-const boundary = canvas.getBoundingClientRect();
-const particles = [];
+const mousePosition = {x: 0, y: 0};
 const pi = Math.PI;
+const resolutionModifier = 3;
 
 class Particle {
 
     constructor(xPosition, yPosition) {
         this._x = xPosition;
         this._y = yPosition;
-        this._speed = Math.floor(9 * Math.random() + 1);
+        this._radius = Math.floor(8 * Math.random() + 2);
+        this._speed = 4 - (this._radius/4);
         this._direction = Math.random() * 2 * pi;
-        this._radius = Math.floor(9 * Math.random() + 1);
-        this._vicinity = 200;
+        this._vicinity = 300;
     }
 
     get x() { return this._x }
@@ -23,23 +23,6 @@ class Particle {
     get radius() { return this._radius }
     get vicinity() { return this._vicinity }
 
-    bounce(edge) {
-        switch (edge) {
-            case 'horizontal':
-                this._direction = (2 * pi - this.direction);
-                break
-            case 'vertical':
-                this._direction = (pi - this.direction);
-                break
-        }
-    }
-
-    render(context) {
-        context.beginPath()
-        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-        context.fill()
-    }
-
     move() {
         let xComponent = this.speed * Math.cos(this.direction);
         let yComponent = this.speed * Math.sin(this.direction);
@@ -47,48 +30,118 @@ class Particle {
         this._y = this.y + yComponent;
     }
 
-    renderEdge(p, context) {
+    collide() {
+        particles.forEach(p => {
+            if(this !== p) {
 
-        const a = this.x - p.x;
-        const b = this.y - p.y;
-        const distance = Math.floor(Math.sqrt(a**2 + b**2));
+                const xDiff = p.x - this.x;
+                const yDiff = p.y - this.y;
+
+                const distance = Math.sqrt(xDiff**2 + yDiff**2);
+                const radii = this.radius + p.radius;
+                const isCollision = (distance <= radii);
+                
+                if(isCollision) {
+
+                    let anglePerp = Math.acos(Math.abs(xDiff/radii));
+                    if(-xDiff*yDiff > 0) {
+                        anglePerp = -anglePerp;
+                    }
+                    if(Math.sign(xDiff) > 0) {
+                        anglePerp = anglePerp + pi;
+                    }
+
+                    this._direction = anglePerp;
+
+                    //currently bouncing off each other at the perpendicular
+                }
+            }
+        })
+    }
+
+    bounce(edge) {
+        switch (edge) {
+            case 'horizontal':
+                this._direction = -this.direction;
+                break
+            case 'vertical':
+                this._direction = pi - this.direction;
+                break
+        }
+    }
+
+    render() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+        ctx.fill()
+    }
+
+    renderEdge() {
+        let index = particles.indexOf(this);
         
-        if(distance < this.vicinity) {
-            const alpha = 0.8 - (distance / (this.vicinity / 0.8));
-            context.strokeStyle = `rgba(255, 255, 255, ${alpha})`;
-            context.lineWidth = Math.sqrt(this.radius + p.radius);
-            context.beginPath();
-            context.moveTo(this.x, this.y);
-            context.lineTo(p.x, p.y);
-            context.stroke()
+        for (let i = index -1; i >= 0; i--) {
+            let p = particles[i];
+            const a = this.x - p.x;
+            const b = this.y - p.y;
+            const distance = Math.floor(Math.sqrt(a**2 + b**2));
+            
+            if(distance < this.vicinity) {
+                const alpha = 0.7 - (distance / (this.vicinity / 0.7));
+                ctx.strokeStyle = `rgba(255, 255, 255, ${alpha})`;
+                ctx.lineWidth = Math.sqrt(this.radius + p.radius);
+                ctx.beginPath();
+                ctx.moveTo(this.x, this.y);
+                ctx.lineTo(p.x, p.y);
+                ctx.stroke()               
+            }
         }
     }
 }
 
-function setContext() {
-    ctx.lineWidth = 1;
-    ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
-    ctx.strokeStyle = 'hsla(0, 0, 100%, 0.5)';
+const particles = [new Particle(100,100), new Particle(500,500), new Particle(1000,1000)];
 
+function setctx() {
+    ctx.lineWidth = 1;
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+    ctx.strokeStyle = 'hsla(0, 0, 100%, 0.5)';
     ctx.save();
 }
 
-function generateParticle(event) {
-    const mousePos = {
-        x: (event.clientX - boundary.left) / (boundary.right - boundary.left) * canvas.width,
-        y: (event.clientY - boundary.top) / (boundary.bottom - boundary.top) * canvas.height
-    };
+function mousePos(event) {
+    const boundary = canvas.getBoundingClientRect();
+    return {
+        x: (event.clientX - boundary.left) * resolutionModifier,
+        y: (event.clientY - boundary.top) * resolutionModifier
+    }
+}
 
-    const newParticle = new Particle(mousePos.x, mousePos.y);
-    newParticle.render(ctx);
+function moveCursor(event,) {
+    const mouse = mousePos(event);
+    mousePosition.x = event.clientX;
+    mousePosition.y = event.clientY;
+}
+
+function generateParticle(event) {
+    const mouse = mousePos(event);
+    console.e
+    const newParticle = new Particle(mouse.x, mouse.y);
     particles.push(newParticle);
+    newParticle.render();
+}
+
+function checkCollisions() {
+    particles.forEach( particle => {
+        particle.collide();
+    })
 }
 
 function nextFrame() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i=0; i < particles.length; i++) {
-        let particle = particles[i];
-        particle.move();
+    setctx();
+    checkCollisions();
+    particles.forEach( particle => {
+        particle.renderEdge();
+
         let withinVertical = (particle.x + particle.radius < canvas.width) && (particle.x - particle.radius > 0);
         let withinHorizontal = (particle.y + particle.radius < canvas.height) && (particle.y - particle.radius > 0);
         if(!withinVertical) {
@@ -97,15 +150,14 @@ function nextFrame() {
         if(!withinHorizontal) {
             particle.bounce('horizontal')
         }
-        particle.render(ctx);
-
-        for (let j = i-1; j >= 0; j--) {
-            particle.renderEdge(particles[j], ctx)
-        }
-    }
+        particle.move();
+        particle.render(ctx);  
+    })
+    
 }
 
-setContext();
-setInterval(nextFrame, 50);
+setctx();
+setInterval(nextFrame, 20);
 
 canvas.addEventListener('click', generateParticle);
+canvas.addEventListener('mousemove', moveCursor);
