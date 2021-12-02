fe73aa1ad30ae928c4b1b48ef599b4e8af6c9ae9
Harry-Purple <haroldholehouse@gmail.com>
2021-12-02 20:41:07 +0000

    Populating ParticleCanvas

--- src/classes/ParticleCanvas.js
+++ src/classes/ParticleCanvas.js
@@ -1,19 +1,46 @@
-// import {} from '';
-//Getting the size of the canvas and assigning it to an object
+import { z, Z, zQuery } from '../../modules/zQuery/z-query';
+import { particleManager } from './'
+zQuery.init( [ 'events' ] )
+//Getting the size of the this and assigning it to an object
 export class ParticleCanvas extends HTMLCanvasElement {
 	constructor() {
 		super()
-		this.widths = this.computedStyle( 'width' ).replace( 'px', '' )
-		this.heights = this.computedStyle( 'height' ).replace( 'px', '' )
+		this.width = this.computedStyle( 'width' ).replace( 'px', '' )
+		this.height = this.computedStyle( 'height' ).replace( 'px', '' )
 		this.bounds = this.getBoundingClientRect()
+		this.pixelDensity = this.getAttribute()
+		this.particleManager = new particleManager()
+		this.on( 'resize', this.createResizeHandler(), { 'passive': true } );
 	}
 	area() {
 		return this.width * this.height
 	}
 	refresh() {
-		this.widths = this.computedStyle( 'width' ).replace( 'px', '' );
-		this.heights = this.computedStyle( 'height' ).replace( 'px', '' );
+		this.width = this.computedStyle( 'width' ).replace( 'px', '' );
+		this.height = this.computedStyle( 'height' ).replace( 'px', '' );
 		this.bounds = this.getBoundingClientRect();
 	}
+	resize() {
+		const oldCanvasSize = { width: this.width, height: this.height };
+		this.refresh();
+
+		const sizeRatio = this.width / oldCanvasSize.width;
+		options.vicinity *= sizeRatio ** 0.5;
+
+		particles.forEach( particle => {
+			particle.x = particle.x * ( this.width / oldCanvasSize.width );
+			particle.y = particle.y * ( this.height / oldCanvasSize.height );
+		} )
+		this.height = Math.floor( options.resolutionModifier * this.height );
+		this.width = Math.floor( options.resolutionModifier * this.width );
+	}
+
+	createResizeHandler() {
+		let resizeId;
+		return () => {
+			clearTimeout( resizeId );
+			resizeId = setTimeout( this.resizeCanvas, 300 );
+		}
+	}
 }
-customElements.define( 'particle-canvas', ParticleCanvas, { extends: 'canvas' } );
+window.customElements.define( 'particle-this', ParticleCanvas, { extends: 'canvas' } );
